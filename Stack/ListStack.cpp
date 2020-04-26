2c0df5909999b37ea472dce78ca7c14e09be4b3e
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-04-26 16:55:21 +0300

    Update ListStack.cpp

--- Stack/ListStack.cpp
+++ Stack/ListStack.cpp
@@ -1,1 +1,39 @@
 #include "ListStack.h"
+#include "LinkedList.h"
+
+ListStack::ListStack(const ListStack& copyList):LinkedList(copyList)
+{
+}
+
+void ListStack::push(const ValueType& value)
+{
+	this->pushBack(value);
+}
+
+void ListStack::pop()
+{
+	this->removeBack();
+}
+
+ValueType& ListStack::top()
+{
+	return operator[](size() - 1);
+}
+
+const ValueType& ListStack::top() const
+{
+	return operator[](size() - 1);
+}
+
+bool ListStack::isEmpty() const
+{
+	if (size() == 0)
+		return true;
+	else
+		return false;
+}
+
+size_t ListStack::size() const
+{
+	return LinkedList::size();
+}
