8dbac0c76c1ddeeef8c9edd424749d71e91c53e9
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-04-26 16:56:31 +0300

    Update ListStack.h

--- Stack/ListStack.h
+++ Stack/ListStack.h
@@ -1,13 +1,26 @@
 #pragma once
 
 #include "StackImplementation.h"
-class LinkedList; // меняете на include вашего листа
+#include "LinkedList.h" // меняете на include вашего листа
 
 // вариант с использованием ранее написанного списка и множественного наследования
 // если бы список не был реализован, то было бы наследование только от интерфейса
 // множественное наследование можно заменить на композицию
 class ListStack : public StackImplementation, public LinkedList
 {
-	// вот тут вы вступаете в дело
+	ListStack() {};
+	ListStack(const ListStack& copyList);
+	void push(const ValueType& value) override;
+	// удаление с хвоста
+	void pop()override;
+	// посмотреть элемент в хвосте
+	ValueType& top()override;
+	const ValueType& top() const override;
+	// проверка на пустоту
+	bool isEmpty() const override;
+	// размер 
+	size_t size() const override;
+	// виртуальный деструктор
+	~ListStack()override {};
 };
 
