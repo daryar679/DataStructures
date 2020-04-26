14a415e37263c034466887280583cd294e037bc5
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-04-26 17:03:59 +0300

    Update VectorStack.cpp

--- Stack/VectorStack.cpp
+++ Stack/VectorStack.cpp
@@ -1,2 +1,39 @@
 #include "VectorStack.h"
+#include "Vector.h"
 
+VectorStack::VectorStack(const VectorStack& copy):MyVector(copy)
+{
+}
+
+void VectorStack::push(const ValueType& value)
+{
+	this->pushBack(value);
+}
+
+void VectorStack::pop()
+{
+	this->popBack();
+}
+
+ValueType& VectorStack::top()
+{
+	return operator[](size() - 1);
+}
+
+const ValueType& VectorStack::top() const
+{
+	return operator[](size() - 1);
+}
+
+bool VectorStack::isEmpty() const
+{
+	if (size() == 0)
+		return true;
+	else
+		return false;
+}
+
+size_t VectorStack::size() const
+{
+	return MyVector::size();
+}
