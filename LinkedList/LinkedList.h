7c9ab706ef91e3fd1bf2e8430162a581a2a5bef6
daryar679 <58885122+daryar679@users.noreply.github.com>
2020-05-01 14:58:48 +0300

    Update LinkedList.h

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -41,7 +41,7 @@ public:
 	// вставка элемента по индексу, сначала ищем, куда вставлять (О(n)), потом вставляем (O(1))
 	void insert(const size_t pos, const ValueType& value);
 	// вставка элемента после узла, (O(1))
-	static void insertAfterNode(Node* node, const ValueType& value);
+	void insertAfterNode(Node* node, const ValueType& value);
 	// вставка в конец (О(n))
 	void pushBack(const ValueType& value);
 	// вставка в начало (О(1))
